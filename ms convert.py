96d5f7bc0d15ca20fca39c329a2eb269680035bf
eattato <80892781+eattato@users.noreply.github.com>
2022-07-28 20:32:16 +0900

    변환기 버그 고침

--- ms convert.py
+++ ms convert.py
@@ -10,8 +10,9 @@ result = open("converted.txt", "w")
 for ind, line in enumerate(lines): # 모든 라인 루프
     split = line.split(",") # ,를 기준으로 나눔
     if len(split) == 4: # 옳은 형식이면
-        split[0] = int(float(split[0]) / 1000) # 타이밍 ms로 수정
+        split[0] = str(int(float(split[0]) * 1000)) # 타이밍 ms로 수정
         lines[ind] = ",".join(split) # 다시 붙여서 라인을 수정
+        print(split)
     lines[ind].strip() # \n같은 거 다 지움
 result.write("\n".join(lines))
 result.close()
